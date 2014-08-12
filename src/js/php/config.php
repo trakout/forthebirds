49e3ab2544a327ab1d78f31018e7b26adcb793bc
Peony Gerochi <itsduc@gmail.com>
2014-08-12 09:43:42 -0400

    no message

--- src/js/php/config.php
+++ src/js/php/config.php
@@ -1,10 +1,9 @@
 <?php
 
 // Go to https://dev.twitter.com/apps/new to get this info.
-
-$consumer_key = '9RgMeTRAz1dbE80Kb3eTAEZHM';
-$consumer_secret = 'JIjDSGFzWgiONPgU2sRyAaJEGYD5ObQfA3GBLhrdR9lTxxt8Or';
-$access_token = '11611672-tPk1s7jHqtLRZmJsmUBZt0NN4DVMTFyLMs3q8tSLS';
-$access_token_secret = '9kDTLXX5nX4LZgakLX8bP1Km4VtDFj0OpTsDzmtIdKqSE';
+$consumer_key = 'YOUR_CONSUMER_KEY';
+$consumer_secret = 'YOUR_CONSUMER_SECRET';
+$access_token = 'YOUR_OAUTH_ACCESS_TOKEN';
+$access_token_secret = 'YOUR_OAUTH_ACCESS_TOKEN_SECRET';
 
 ?>
